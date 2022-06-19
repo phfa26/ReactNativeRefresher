259de28b83ccf6123290cc3371617a325f615bcc
phfa26 <phfa@outlook.com.au>
2022-06-19 19:46:48 +1000

    Initial App Setting

--- Project1/App.js
+++ Project1/App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	FlatList,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+	const [goalInput, setGoalInput] = useState("");
+	const [goals, setGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setGoalInput(enteredText);
+	};
+	const addGoalHandler = () => {
+		setGoals((currentGoals) => [...currentGoals, goalInput]);
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.textInput}
+					placeholder="Enter your goal here!"
+					onChangeText={goalInputHandler}
+				></TextInput>
+				<Button onPress={addGoalHandler} title="Add Goal"></Button>
+			</View>
+			<View style={styles.goalsContainer}>
+				<FlatList
+					alwaysBounceVertical={false}
+					data={goals}
+					keyExtractor={(item, index) => {
+						return index;
+					}}
+					renderItem={(itemData) => {
+						return (
+							<View style={styles.goalItem}>
+								<Text style={styles.goalItemText}>{itemData.item}</Text>
+							</View>
+						);
+					}}
+				/>
+			</View>
 		</View>
 	);
-}
+};
 
 const styles = StyleSheet.create({
-	container: {
+	appContainer: {
+		paddingTop: 50,
+		paddingHorizontal: 16,
 		flex: 1,
-		backgroundColor: "#fff",
+	},
+	inputContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
 		alignItems: "center",
-		justifyContent: "center",
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: "#cccccc",
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: "#cccccc",
+		width: "70%",
+		marginRight: 8,
+		padding: 8,
+	},
+	goalsContainer: {
+		flex: 5,
+	},
+	goalItem: {
+		margin: 8,
+		padding: 8,
+		borderRadius: 6,
+		backgroundColor: "#5e0acc",
+	},
+	goalItemText: {
+		color: "white",
 	},
 });
+
+export default App;
