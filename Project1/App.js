57f8d44a47053c5f3b318dbf2b768337ad391ad7
phfa26 <phfa@outlook.com.au>
2022-06-19 20:27:25 +1000

    Moves button into GoalInput component

--- Project1/App.js
+++ Project1/App.js
@@ -21,10 +21,7 @@ const App = () => {
 
 	return (
 		<View style={styles.appContainer}>
-			<View style={styles.inputContainer}>
-				<GoalInput onChange={ goalInputHandler } />
-				<Button onPress={addGoalHandler} title="Add Goal"></Button>
-			</View>
+			<GoalInput onChange={ goalInputHandler } onPress={addGoalHandler} />
 			<View style={styles.goalsContainer}>
 				<FlatList
 					alwaysBounceVertical={false}
@@ -48,15 +45,6 @@ const styles = StyleSheet.create({
 		paddingTop: 50,
 		paddingHorizontal: 16,
 		flex: 1,
-	},
-	inputContainer: {
-		flex: 1,
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		marginBottom: 24,
-		borderBottomWidth: 1,
-		borderBottomColor: "#cccccc",
 	},
 	goalsContainer: {
 		flex: 5,
--- Project1/components/GoalInput.js
+++ Project1/components/GoalInput.js
@@ -1,7 +1,14 @@
-import {TextInput,StyleSheet} from "react-native";
+import { TextInput, StyleSheet, View, Button } from "react-native";
 
-const GoalInput = ( props ) =>{ 
-    return <TextInput style={styles.textInput} placeholder="Enter your goal here!" onChangeText={ props.onChange }/>
+const GoalInput = ( props ) =>
+{ 
+	
+	return (
+		<View style={ styles.inputContainer }>
+			<TextInput style={ styles.textInput } placeholder="Enter your goal here!" onChangeText={ props.onChange } />
+			<Button onPress={ props.onPress } title="Add Goal"></Button>
+		</View >
+	)
 };
 
 const styles = StyleSheet.create({
@@ -11,6 +18,15 @@ const styles = StyleSheet.create({
 		width: "70%",
 		marginRight: 8,
 		padding: 8,
+	},
+		inputContainer: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		marginBottom: 24,
+		borderBottomWidth: 1,
+		borderBottomColor: "#cccccc",
 	},
 });
 export default GoalInput;
